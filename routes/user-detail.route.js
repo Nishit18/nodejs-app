12ffe2b00faff5f02f1845653d0a3cdc14e7a94b
Nishit18 <patelnishit_18@yahoo.in>
2020-09-06 17:47:11 +0530

    Remove unwanted files

--- routes/user-detail.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-var router = express.Router();
-
-const userDetail = require('../controllers/user-detail.controller');
-
-router.get('/all', userDetail.getUserDetailUsingProcedure);
-
-module.exports = router;
--- routes/user.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-var router = express.Router();
-
-const book = require('../controllers/user.controller');
-
-router.get('/get-user', book.getUserList);
-
-module.exports = router;
