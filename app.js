d14d60529341e72762b0dd10c2fd04504449faa1
Nishit18 <patelnishit_18@yahoo.in>
2020-11-15 12:39:02 +0530

    Add logic for enable cors policy in app js file

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ app.listen(process.env.PORT || 3000,()=>{
     console.log(`Server running`);
 });
 
+// For enable cors policy
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS, PUT"
+    );
+    next();
+});
+
 // For convert stream request data to json object 
 // If you remove this then you can't get data in request body
 app.use(express.json());
